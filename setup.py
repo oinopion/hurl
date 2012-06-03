ce19d85da01ae89b24135f8768b85452ab6010c8
Tomek Paczkowski <tomek@hauru.eu>
2012-06-03 19:23:56 +0200

    Add license and setup py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='hurl',
+    version='1.0',
+    packages=['hurl'],
+    license=open('LICENSE.txt').read(),
+    long_description=open('README.txt').read(),
+)
