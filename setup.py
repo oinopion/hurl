905174f75134a4c7d88d724917e16a534361775b
Tomek Paczkowski <tomek@hauru.eu>
2012-06-07 16:00:01 +0200

    Setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from distutils.core import setup
+from os import path
+
+ROOT = path.dirname(__file__)
+README = path.join(ROOT, 'README.rst')
+REQUIREMENTS = path.join(ROOT, 'requirements.txt')
 
 setup(
     name='hurl',
@@ -6,9 +11,8 @@ setup(
     url='https://github.com/oinopion/hurl',
     author='Tomek Paczkowski & Aleksandra Sendecka',
     author_email='tomek@hauru.eu',
-    version='1.0',
-#    packages=['hurl'],
+    version='1.1.dev',
     license='New BSD license',
-    long_description='',
-    install_requires='django >= 1.3',
+    long_description=open(README).read(),
+    install_requires=open(REQUIREMENTS).read(),
 )
