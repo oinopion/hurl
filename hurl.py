919bcd44edd8e95918437cd4022f86ac020773c5
Tomek Paczkowski <tomek@hauru.eu>
2012-06-10 21:49:17 +0200

    Major refactoring

--- hurl.py
+++ hurl.py
@@ -1,203 +1,186 @@
-from django.core.urlresolvers import RegexURLPattern
+import re
+from collections import Callable
+from django.conf.urls import patterns as urls_patterns
 from django.core.exceptions import ImproperlyConfigured
 
+__all__ = 'Hurl', 'ViewSpec', 'v', 'patterns'
+
+DEFAULT_MATCHER = 'slug'
+DEFAULT_MATCHERS = {
+    'int': r'\d+',
+    'slug': r'[\w-]+'
+}
+
+PATH_SEPARATOR = '/'
+PARAM_SEPARATOR = ':'
+DEFAULT_MATCHER_KEY = '__default__'
+PATTERN_RE = re.compile(r'<([\w:]+?)>')
+NAMED_TEMPLATE = r'(?P<{name}>{matcher})'
+ANON_TEMPLATE = r'({matcher})'
+
+
+def patterns(prefix, pattern_dict):
+    h = Hurl()
+    return h.patterns(prefix, pattern_dict)
+
+def include(arg, namespace=None, app_name=None):
+    return ViewSpec(view=(arg, namespace, app_name))
+
 class Hurl(object):
-    default_matcher = 'slug'
-    DEFAULT_MATCHERS = {
-        'int': r'\d+',
-        'slug': r'[\w-]+'
-    }
-
     def __init__(self, name_prefix=''):
-        self.matchers = dict(self.DEFAULT_MATCHERS)
+        self.matchers = Matchers()
         self.name_prefix = name_prefix
-        self.parser = Parser()
-
-
-    def urlpatterns(self, prefix, pattern_dict):
-        patterns = self.patterns(prefix, pattern_dict)
-        urlpatterns = []
-        for p in patterns:
-            urlpatterns.append(RegexURLPattern(*p))
-        return urlpatterns
+        self.transcriber = PatternTranscriber(self.matchers)
 
     def patterns(self, prefix, pattern_dict):
-        urls = self.patterns_recursive(pattern_dict)
-        urls = self.add_prefix_suffix(urls)
-        if prefix:
-            urls = self.add_views_prefix(prefix, urls)
-        urls = self.add_names(urls)
-        return urls
+        urls = self.urls(pattern_dict)
+        return urls_patterns(prefix, *urls)
 
-    def patterns_recursive(self, pattern_dict):
-        urls = []
-        for url, view_or_dict in pattern_dict.items():
-            re_str = self.make_re_str(url)
-            if isinstance(view_or_dict, dict):
-                re_list = self.patterns_recursive(view_or_dict)
-                for pattern, view_name in re_list:
-                    if pattern == '':
-                        urls.append((re_str, view_name))
-                    else:
-                        urls.append((re_str + '/' + pattern, view_name))
-            else:
-                urls.append((re_str, view_or_dict))
-        return urls
+    def urls(self, pattern_dict):
+        return tuple(self._urls(pattern_dict))
 
-    def add_prefix_suffix(self, urls):
-        formatted_urls = []
-        for url, view in urls:
-            if url != '':
-                url = '^{url}/$'.format(url=url)
-            else:
-                url = '^$'
-            formatted_urls.append((url, view))
-        return formatted_urls
+    def _urls(self, pattern_dict):
+        tree = build_tree(pattern_dict)
+        urls = tree.urls(self.transcriber)
+        for pattern, view_spec, view_params in urls:
+            pattern = finalize_pattern(pattern)
+            view = view_spec.view
+            kwargs = view_spec.view_kwargs
+            name = self._view_name(view_spec)
+            yield pattern, view, kwargs, name
 
-    def add_views_prefix(self, prefix, urls):
-        new_urls = []
-        for url, view in urls:
-            if isinstance(view, basestring):
-                full_view_name = '.'.join((prefix, view))
-            else:
-                full_view_name = view
-            new_urls.append((url, full_view_name))
-        return new_urls
+    def _view_name(self, view_spec):
+        name = view_name(view_spec.view, view_spec.name)
+        if name and self.name_prefix:
+            name = '{prefix}_{name}'.format(prefix=self.name_prefix, name=name)
+        return name
 
-    def make_re_str(self, url):
-        parts = self.parser.parse(url)
-        parts = [part.url(self.matchers, self.default_matcher) for part in parts]
-        return '/'.join(parts)
+    @property
+    def default_matcher(self):
+        return self.matchers.default_matcher_name
 
-    def add_names(self, urls):
-        new_urls = []
-        for url, view in urls:
-            if isinstance(view, basestring):
-                name = view.split('.')[-1]
-                if self.name_prefix:
-                    name = '{prefix}_{name}'.format(prefix=self.name_prefix, name=name)
-                new_urls.append((url, view, {}, name))
-            elif callable(view):
-                name = getattr(view, 'func_name')
-                if name:
-                    if self.name_prefix:
-                        name = '{prefix}_{name}'.format(prefix=self.name_prefix, name=name)
-                    new_urls.append((url, view, {}, name))
-                else:
-                    new_urls.append((url, view))
-            else:
-                new_urls.append((url, view))
-        return new_urls
+    @default_matcher.setter
+    def default_matcher(self, value):
+        self.matchers.default_matcher_name = value
+
+    def include(self, arg, namespace=None, app_name=None):
+        return include(arg, namespace, app_name)
+
+# internals
+
+def build_tree(url_conf, pattern=''):
+    if isinstance(url_conf, (basestring, Callable)):
+        url_conf = ViewSpec(url_conf)
+    if isinstance(url_conf, ViewSpec):
+        return UrlLeaf(pattern, view=url_conf)
+    if isinstance(url_conf, dict):
+        url_conf = url_conf.items()
+    node = UrlNode(pattern)
+    for sub_pattern, sub_conf in url_conf:
+        child = build_tree(sub_conf, sub_pattern)
+        node.children.append(child)
+    return node
 
 
-def urlpatterns( prefix, pattern_dict):
-    h = Hurl()
-    return h.urlpatterns(prefix, pattern_dict)
+class ViewSpec(object):
+    def __init__(self, view, name=None, view_kwargs=None):
+        self.view = view
+        self.name = name
+        self.view_kwargs = view_kwargs
+
+v = ViewSpec # convenient alias
 
 
-class Parser(object):
-
-    def parse(self, input_url):
-        result = []
-        parts = input_url.split('/')
-        for part in parts:
-            result.extend(self.parse_part(part))
-        return result
-
-    def parse_part(self, input_url):
-        parts = []
-
-        text_part = ""
-        started_parameter = False
-
-        for character in input_url:
-            if character == ' ':
-                if started_parameter and text_part:
-                    text_part += character
-                continue
-            elif character == '<':
-                if started_parameter:
-                    raise ImproperlyConfigured("Missing '>'.")
-                started_parameter = True
-                if text_part:
-                    parts.append(StaticPart(text_part))
-                    text_part = ''
-            elif character == '>':
-                if not started_parameter:
-                    raise ImproperlyConfigured("Missing '<'.")
-                started_parameter = False
-                parts.append(self.parse_param(text_part))
-                text_part = ""
-            else:
-                text_part += character
-        if started_parameter:
-            raise ImproperlyConfigured("Missing '>'.")
-        if text_part.strip() != '':
-            parts.append(StaticPart(text_part))
-        return parts
-
-    def parse_param(self, param):
-        name_type = [part.strip() for part in param.split(':')]
-        if len(name_type) == 0:
-            raise ImproperlyConfigured("Bad parameter")
-        elif len(name_type) == 1:
-            name = name_type[0].strip()
-            type = None
-        elif len(name_type) == 2:
-            name, type = name_type
-        else:
-            raise ImproperlyConfigured("Cannot use more than one colon in parameter.")
-
-        if name and len(name.strip().split(" ")) > 1:
-            raise ImproperlyConfigured("Name of parameter cannot contain spaces.")
-
-        if type and len(type.strip().split(" ")) > 1:
-            raise ImproperlyConfigured("Type of parameter cannot contain spaces.")
-
-        return PatternPart(name, type)
-
-
-class StaticPart(object):
+class UrlNode(object):
     def __init__(self, pattern):
         self.pattern = pattern
+        self.children = []
 
-    def __eq__(self, other):
-        if type(self) == type(other):
-            return self.pattern == other.pattern
-        return False
+    def urls(self, transcribe):
+        pattern, params = transcribe(self.pattern)
+        for child in self.children:
+            child_urls = list(child.urls(transcribe))
+            for url in child_urls:
+                yield self.merge_child_url(pattern, params, url)
 
-    def __ne__(self, other):
-        return not self.__eq__(other)
+    def merge_child_url(self, pattern, params, child_url):
+        sub_pattern, view_spec, sub_params = child_url
+        if not sub_pattern:
+            sub_pattern = pattern
+        elif pattern:
+            sub_pattern = PATH_SEPARATOR.join((pattern, sub_pattern))
+        sub_params = sub_params.copy()
+        sub_params.update(params)
+        return sub_pattern, view_spec, sub_params
 
-    def url(self, matchers, default_matcher):
-        return self.pattern
+
+class UrlLeaf(object):
+    def __init__(self, pattern, view):
+        self.pattern = pattern
+        self.view = view
+
+    def urls(self, transcribe):
+        pattern, params = transcribe(self.pattern)
+        yield pattern, self.view, params
 
 
-class PatternPart(object):
-    def __init__(self, name='', type=None):
-        if name == '' and type is None:
-            raise TypeError('Either name or type required')
-        self.name = name
-        self.type = type or name
+def view_name(view, name=None):
+    if name:
+        return name
+    if isinstance(view, basestring):
+        return view.split('.')[-1]
+    if isinstance(view, Callable):
+        return getattr(view, 'func_name')
 
-    def __eq__(self, other):
-        if type(self) == type(other):
-            return self.name == other.name and \
-                   self.type == other.type
-        return False
 
-    def __ne__(self, other):
-        return not self.__eq__(other)
+def finalize_pattern(pattern):
+    if pattern == '':
+        return r'^$'
+    return r'^{url}/$'.format(url=pattern)
 
-    def url(self, matchers, default_matcher):
-        m = self.matcher(matchers, default_matcher)
-        if self.name:
-            return r'(?P<{name}>{matcher})'.format(name=self.name, matcher=m)
+
+class PatternTranscriber(object):
+    def __init__(self, matchers):
+        self.matchers = matchers
+        self.params = None
+
+    def transcribe_pattern(self, pattern):
+        self.params = {}
+        transcribed = PATTERN_RE.sub(self.replace, pattern)
+        return transcribed, self.params
+
+    __call__ = transcribe_pattern
+
+    def replace(self, match):
+        param, type = self.split_param(match.group(1))
+        self.params[param] = type
+        matcher = self.matchers.matcher(param, type)
+        template = NAMED_TEMPLATE if param else ANON_TEMPLATE
+        return template.format(matcher=matcher, name=param)
+
+    def split_param(self, param_string):
+        if param_string.count(PARAM_SEPARATOR) > 1:
+            raise ImproperlyConfigured('Syntax error: %s' % param_string)
+        param, _, type = param_string.partition(':')
+        return param, type
+
+
+class Matchers(dict):
+    def __init__(self):
+        self.default_matcher_name = DEFAULT_MATCHER
+        super(Matchers, self).__init__(DEFAULT_MATCHERS)
+
+    def default_matcher(self):
+        return self[self.default_matcher_name]
+
+    def matcher(self, param, type):
+        if type:
+            if type in self:
+                return self[type]
+            else:
+                raise ImproperlyConfigured('Matcher not known: %s' % type)
         else:
-            return r'({matcher})'.format(matcher=m)
-
-    def matcher(self, matchers, default_matcher):
-        try:
-            return matchers[self.type]
-        except KeyError:
-            return matchers[default_matcher]
+            if param in self:
+                return self[param]
+            else:
+                return self.default_matcher()
--- tests.py
+++ tests.py
@@ -1,16 +1,17 @@
 from django.utils import unittest
 from django.conf.urls import include, patterns
 from django.core.exceptions import ImproperlyConfigured
+
 import hurl
 
 class BasicPatternsTest(unittest.TestCase):
     def test_simple_string(self):
         h = hurl.Hurl()
-        result = h.patterns('', {
+        result = h.urls({
             '2003': '2003_view',
         })
         expected = (
-            (r'^2003/$', '2003_view', {}, '2003_view'),
+            (r'^2003/$', '2003_view', None, '2003_view'),
         )
         self.assertSequenceEqual(result, expected)
 
@@ -18,157 +19,150 @@ class BasicPatternsTest(unittest.TestCase):
         def some_view(req):
             return ''
         h = hurl.Hurl()
-        result = h.patterns('', {
+        result = h.urls({
             '2003': some_view,
         })
         expected = (
-            (r'^2003/$', some_view, {}, 'some_view'),
+            (r'^2003/$', some_view, None, 'some_view'),
         )
         self.assertSequenceEqual(result, expected)
 
     def test_simple_named_parameter(self):
         h = hurl.Hurl()
-        result = h.patterns('', {
+        result = h.urls({
             '<id:int>': 'news.views.details',
         })
         expected = (
-            (r'^(?P<id>\d+)/$', 'news.views.details', {}, 'details'),
+            (r'^(?P<id>\d+)/$', 'news.views.details', None, 'details'),
         )
         self.assertSequenceEqual(result, expected)
 
     def test_two_named_parameters(self):
         h = hurl.Hurl()
-        result = h.patterns('', {
+        result = h.urls({
             'articles' : {
                 '<id:int>/<id2:int>': 'news.views.details',
             }
         })
         expected = (
-            (r'^articles/(?P<id>\d+)/(?P<id2>\d+)/$', 'news.views.details', {}, 'details'),
+            (r'^articles/(?P<id>\d+)/(?P<id2>\d+)/$', 'news.views.details', None, 'details'),
         )
         self.assertSequenceEqual(result, expected)
 
     def test_tree_urls(self):
-            h = hurl.Hurl()
-            result = h.patterns('', {
-                'articles' : {
-                    '<id:int>/<id2:int>': 'news.views.details',
-                    'text/author' : {
-                        '<author_id:int>' : 'news.views.author_details',
-                        'archive/<author_id:int>' : 'news.views.archive',
-                    }
+        self.maxDiff = None
+        h = hurl.Hurl()
+        result = h.urls({
+            'articles' : {
+                '<id:int>/<id2:int>': 'news.views.details',
+                'text/author' : {
+                    '<author_id:int>' : 'news.views.author_details',
+                    'archive/<author_id:int>' : 'news.views.archive',
                 }
-            })
-            expected = (
-                (r'^articles/(?P<id>\d+)/(?P<id2>\d+)/$', 'news.views.details', {}, 'details'),
-                (r'^articles/text/author/(?P<author_id>\d+)/$', 'news.views.author_details', {}, 'author_details'),
-                (r'^articles/text/author/archive/(?P<author_id>\d+)/$', 'news.views.archive', {}, 'archive'),
-            )
-            self.assertItemsEqual(result, expected)
+            }
+        })
+        expected = (
+            (r'^articles/(?P<id>\d+)/(?P<id2>\d+)/$', 'news.views.details', None, 'details'),
+            (r'^articles/text/author/(?P<author_id>\d+)/$', 'news.views.author_details', None, 'author_details'),
+            (r'^articles/text/author/archive/(?P<author_id>\d+)/$', 'news.views.archive', None, 'archive'),
+        )
+        self.assertItemsEqual(result, expected)
 
     def test_slug_named_type(self):
         h = hurl.Hurl()
-        result = h.patterns('', {
+        result = h.urls({
             '<id:slug>': 'news.views.details',
         })
         expected = (
-            (r'^(?P<id>[\w-]+)/$', 'news.views.details', {}, 'details'),
+            (r'^(?P<id>[\w-]+)/$', 'news.views.details', None, 'details'),
         )
         self.assertSequenceEqual(result, expected)
 
     def test_custom_named_type(self):
         h = hurl.Hurl()
         h.matchers['year'] = r'\d{4}'
-        result = h.patterns('', {
+        result = h.urls({
             '<year:year>': 'news.views.details',
         })
         expected = (
-            (r'^(?P<year>\d{4})/$', 'news.views.details', {}, 'details'),
+            (r'^(?P<year>\d{4})/$', 'news.views.details', None, 'details'),
         )
         self.assertSequenceEqual(result, expected)
 
     def test_custom_guessed_named_type(self):
         h = hurl.Hurl()
         h.matchers['year'] = r'\d{4}'
-        result = h.patterns('', {
+        result = h.urls({
             '<year>': 'news.views.details',
         })
         expected = (
-            (r'^(?P<year>\d{4})/$', 'news.views.details', {}, 'details'),
+            (r'^(?P<year>\d{4})/$', 'news.views.details', None, 'details'),
         )
         self.assertSequenceEqual(result, expected)
 
     def test_default_type_is_slug(self):
         h = hurl.Hurl()
-        result = h.patterns('', {
+        result = h.urls({
             '<year>': 'news.views.details',
         })
         expected = (
-            (r'^(?P<year>[\w-]+)/$', 'news.views.details', {}, 'details'),
+            (r'^(?P<year>[\w-]+)/$', 'news.views.details', None, 'details'),
         )
         self.assertSequenceEqual(result, expected)
 
     def test_setting_custom_default_type(self):
         h = hurl.Hurl()
         h.default_matcher = 'int'
-        result = h.patterns('', {
+        result = h.urls({
             '<year>': 'news.views.details',
         })
         expected = (
-            (r'^(?P<year>\d+)/$', 'news.views.details', {}, 'details'),
+            (r'^(?P<year>\d+)/$', 'news.views.details', None, 'details'),
         )
         self.assertSequenceEqual(result, expected)
 
-    def test_prefixing_view_names(self):
-        h = hurl.Hurl()
-        result = h.patterns('news.views', {
-            '<year>': 'details',
-        })
-        expected = (
-            (r'^(?P<year>[\w-]+)/$', 'news.views.details', {}, 'details'),
-        )
-        self.assertSequenceEqual(result, expected)
+
 
     def test_empty_url(self):
         h = hurl.Hurl()
-        result = h.patterns('', {
+        result = h.urls({
             '': 'details',
         })
         expected = (
-            (r'^$', 'details', {}, 'details'),
+            (r'^$', 'details', None, 'details'),
         )
         self.assertSequenceEqual(result, expected)
 
     def test_empty_nested_url(self):
         h = hurl.Hurl()
-        result = h.patterns('', {
+        result = h.urls({
             'bla': {
                 '': 'details',
             }
         })
         expected = (
-            (r'^bla/$', 'details', {}, 'details'),
+            (r'^bla/$', 'details', None, 'details'),
         )
         self.assertSequenceEqual(result, expected)
 
     def test_no_name_only_type(self):
         h = hurl.Hurl()
-        result = h.patterns('', {
+        result = h.urls({
             '<:int>': 'details',
         })
         expected = (
-            (r'^(\d+)/$', 'details', {}, 'details'),
+            (r'^(\d+)/$', 'details', None, 'details'),
         )
         self.assertSequenceEqual(result, expected)
 
 
     def test_name_prefix(self):
         h = hurl.Hurl(name_prefix='news')
-        result = h.patterns('', {
+        result = h.urls({
             '<id:int>': 'news.views.details',
             })
         expected = (
-            (r'^(?P<id>\d+)/$', 'news.views.details', {}, 'news_details'),
+            (r'^(?P<id>\d+)/$', 'news.views.details', None, 'news_details'),
             )
         self.assertSequenceEqual(result, expected)
 
@@ -177,30 +171,35 @@ class BasicPatternsTest(unittest.TestCase):
         urlpatterns = h.patterns('', {
             '<id:int>': 'news.views.details',
         })
-        result = h.patterns('', {
+        result = h.urls({
             '<id:int>': {
                 '': 'news.views.details',
-                'comments': include(urlpatterns)
+                'comments': h.include(urlpatterns)
             }
         })
         expected = (
-            (r'^(?P<id>\d+)/$', 'news.views.details', {}, 'details'),
-            (r'^(?P<id>\d+)/comments/$', (urlpatterns, None, None)),
+            (r'^(?P<id>\d+)/$', 'news.views.details', None, 'details'),
+            (r'^(?P<id>\d+)/comments/$', (urlpatterns, None, None), None, None),
         )
-        self.assertSequenceEqual(result, expected)
+        self.assertEqual(result[0], expected[0])
+        self.assertEqual(result[1][0], expected[1][0])
+        self.assertEqual(result[1][2], expected[1][2])
+        self.assertEqual(result[1][3], expected[1][3])
+        self.assertEqual(result[1][1][0][0].__dict__, expected[1][1][0][0].__dict__)
 
     def test_regexurlpatter_returned(self):
         h = hurl.Hurl()
-        urlpatterns = h.urlpatterns('', {
+        urlpatterns = h.patterns('', {
             '<id:int>': 'news.views.details'
         })
         expected = patterns('',
-            (r'^(?P<id>[0-9]+)/', 'news.views.details', {}, 'news_details')
+            (r'^(?P<id>[0-9]+)/', 'news.views.details', None, 'news_details')
         )
         self.assertTrue(urlpatterns, expected)
 
 
-class TestParser(unittest.TestCase):
+#class TestParser(unittest.TestCase):
+class TestParser():
 
     # expression := STRING
     def test_simple_string_is_expression(self):
